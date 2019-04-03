6f03830b4a0664c2886c1b5f35bf74de66fcb3a9
bigand32 <bigand32@gmail.com>
2019-04-04 00:43:59 +0900

    gf

---
+++ app/src/main/java/com/example/sosimtapa/pid.java
@@ -0,0 +1,105 @@
+package com.example.sosimtapa;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class pid extends Activity {
+    TextView selectedText;
+    Spinner spinner;
+    Spinner spinner1;
+    String[] item;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add);
+
+        Button button=(Button) findViewById(R.id.add);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),write.class);
+                startActivity(intent);
+            }
+        });
+        spinner = (Spinner) findViewById(R.id.spinner1);
+        // spinner.setOnItemSelectedListener(this);
+        item = new String[]{"강서구", "관악구", "동작구", "영등포구", "성북구"};
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner2);
+        // spinner.setOnItemSelectedListener(this);
+        item = new String[]{"강서구", "관악구", "동작구", "영등포구", "성북구"};
+
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(adapter1);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter((ListAdapter) new ImageAdapter(this));
+    }
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+        private Integer[] mThumblds= {R.drawable.b,R.drawable.b,R.drawable.b,R.drawable.b,R.drawable.b,R.drawable.b,R.drawable.b,R.drawable.b,R.drawable.b,R.drawable.b};
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+        public int getCount() {
+            return mThumblds.length;
+        }
+        public Object getItem(int position) {
+            return null;
+        }
+        public long getItemId(int position) {
+            return 0;
+        }
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(350,350));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            }else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(mThumblds[position]);
+            return imageView;
+        }
+
+
+    }
+}
+   /* @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        selectedText.setText(item[i]);
+        if (selectedText.getText().toString().equals("관악구")) {
+            selectedText.setText("");
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        selectedText.setText("");
+    }*/
---
+++ app/src/main/java/com/example/sosimtapa/write.java
@@ -0,0 +1,66 @@
+package com.example.sosimtapa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.io.InputStream;
+
+public class write extends Activity {
+    ImageView imageView;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adding);
+
+        //final TextView tv = (TextView) findViewById(R.id.textView1); //별점 몇점인지 나타낼때사용
+        RatingBar rb =(RatingBar)findViewById(R.id.ratingBar1);
+
+        rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
+               // tv.setText("rating : " + rating);
+            }
+        });
+        imageView = (ImageView)findViewById(R.id.image);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                try {
+                    // 선택한 이미지에서 비트맵 생성
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    // 이미지 표시
+                    imageView.setImageBitmap(img);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
