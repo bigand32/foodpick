073bee1dbb5c8168ff88d56d7b9479af8b7cd4fb
Minji Bake <bigand32@gmail.com>
2019-04-11 18:52:37 +0900

    "커밋"

--- app/src/main/java/com/example/sosimtapa/newsign.java
+++ app/src/main/java/com/example/sosimtapa/newsign.java
@@ -1,12 +1,24 @@
 package com.example.sosimtapa;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class newsign extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newpeople);
+
+        Button button1=(Button) findViewById(R.id.btn_result);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sosimtapa/pid.java
+++ app/src/main/java/com/example/sosimtapa/pid.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -37,7 +36,7 @@ public class pid extends Activity {
         });
         spinner = (Spinner) findViewById(R.id.spinner1);
         // spinner.setOnItemSelectedListener(this);
-        item = new String[]{"강서구", "관악구", "동작구", "영등포구", "성북구"};
+        item = new String[]{"강남구", "관악구", "동작구", "영등포구", "성북구"};
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, item);
@@ -47,7 +46,7 @@ public class pid extends Activity {
 
         spinner1 = (Spinner) findViewById(R.id.spinner2);
         // spinner.setOnItemSelectedListener(this);
-        item = new String[]{"여의도", "신림", "서울대입구", "영등포구", "성북구"};
+        item = new String[]{"역삼동", "신림", "서울대입구", "영등포구", "성북구"};
 
 
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, item);
