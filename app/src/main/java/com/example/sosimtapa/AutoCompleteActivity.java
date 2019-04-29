af3822ed195972a1ac619c93839c6be958f1886d
Minji Bake <bigand32@gmail.com>
2019-04-29 20:22:07 +0900

    "자동완성"

--- app/src/main/java/com/example/sosimtapa/AutoCompleteActivity.java
+++ app/src/main/java/com/example/sosimtapa/AutoCompleteActivity.java
@@ -1,42 +1,57 @@
 package com.example.sosimtapa;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
-public class AutoCompleteActivity extends Activity {
-    private TextView txtSelectedPlaceName;
+import java.util.Arrays;
+import java.util.List;
 
+public class AutoCompleteActivity extends AppCompatActivity {
+
+    PlacesClient placesClient;
+    List<Place.Field> placeFields= Arrays.asList(Place.Field.ID,
+            Place.Field.NAME,
+            Place.Field.ADDRESS);
+    AutocompleteSupportFragment places_fragment;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autocomplete);
 
-        txtSelectedPlaceName = (TextView) this.findViewById(R.id.txtSelectedPlaceName);
+        initPlaces();
 
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.fragment_autocomplete);
+        setupPlaceAutoComplete();
+    }
 
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+    private void setupPlaceAutoComplete() {
+        places_fragment = (AutocompleteSupportFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.places_autocomplete_fragment);
+        places_fragment.setPlaceFields(placeFields);
+        places_fragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
-            public void onPlaceSelected(Place place) {
-
-                txtSelectedPlaceName.setText(String.format("Selected places : %s  - %s" , place.getName(), place.getAddress()));
+            public void onPlaceSelected(@NonNull Place place) {
+                Toast.makeText(AutoCompleteActivity.this, ""+place.getName(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onError(Status status) {
+            public void onError(@NonNull Status status) {
+                Toast.makeText(AutoCompleteActivity.this,""+status.getStatusMessage(), Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(AutoCompleteActivity.this, "Place cannot be selected!!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    private void initPlaces() {
+        Places.initialize(this,getString(R.string.place_api_key));
+        placesClient = Places.createClient(this);
     }
 }
--- app/src/main/java/com/example/sosimtapa/newsign.java
+++ app/src/main/java/com/example/sosimtapa/newsign.java
@@ -12,7 +12,16 @@ public class newsign extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newpeople);
 
-        Button button1=(Button) findViewById(R.id.btn_result);
+        Button button1=(Button) findViewById(R.id.btn_reset);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2=(Button) findViewById(R.id.btn_result);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
